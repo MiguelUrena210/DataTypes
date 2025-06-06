e217d240c8a0a582cf38b8161094e55713717afd
Miguel <miguel02urena@gmail.com>
2025-06-06 08:16:36 -0600

    first commit

---
+++ susbcripting.py
@@ -0,0 +1,23 @@
+# Notes
+
+# first letter 
+print("hello"[0])
+
+# last letter
+print('hello'[-1])
+
+# String concatenation
+print("123" + "345") # quotes make it a string not a int
+
+# Integer = whole number
+print(123 + 345)
+
+# Print large numbers
+print(10_000) 
+
+# float = floating point number
+print(3.14159)
+
+# Boolean
+print(True) # always with capital letter
+print(False)
